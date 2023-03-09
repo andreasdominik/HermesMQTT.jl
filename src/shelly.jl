07c09deab7efa7e855b830707590dc6610129ffa
Andreas Dominik <andreas.dominik@mni.thm.de>
2023-03-09 17:16:31 +0100

    shelly with HTTP.jlshelly tested

--- src/shelly.jl
+++ src/shelly.jl
@@ -5,7 +5,8 @@
 
 
 """
-    switch_shelly_1(ip, action; timer = 10)
+    switch_shelly_1(ip, action; 
+            port=nothing, user=nothing, password=nothing)
 
 Switch a shelly1 device with IP `ip` on or off, depending
 on the value given as action and
@@ -16,17 +17,19 @@ return `true`, if successful.
 - `action`: demanded action as symbol; one of `:on`, `:off`, `:push` or `:timer`.
             action `:push` will switch on for 200ms to simulate a push.
             action `:timer` will switch off after timer secs.
-- `timer`: if action == `timer`, the device is switched on and
-           off again after `timer` secs (default 10s).
+- `port`, `user`, `password`: if given, the device is accessed via
+           http://user:password@ip:port
 
 For the API-doc of the Shelly devices see:
 `<https://shelly-api-docs.shelly.cloud>`.
 """
-function switch_shelly_1(ip, action; timer = 10)
+function switch_shelly_1(ip, action; 
+            port=nothing, user=nothing, password=nothing)
 
-    success = switch_shelly_25_relay(ip, 0, action; timer = timer)
+    success = switch_shelly_25_relay(ip, 0, action; 
+                port=port, user=user, password=password)
     if !success
-        print_log("ERROR in switchShelly1 with ip $ip and action $action")
+        print_log("ERROR in switch_shelly_1 with ip $ip and action $action")
         publish_say("Try to switch a Shelly-one was not successful!")
     end
 
@@ -36,7 +39,8 @@ end
 
 
 """
-    switch_shelly_25_relay(ip, relay, action; timer = 10)
+    switch_shelly_25_relay(ip, relay, action; 
+            port=nothing, user=nothing, password=nothing)
 
 Switch the relay `relay` of a shelly2.5 device with IP `ip` on or off, depending
 on the value given as action and return `true`, if successful.
@@ -46,45 +50,57 @@ on the value given as action and return `true`, if successful.
 - `relay`: Number of relay to be switched (0 or 1)
 - `action`: demanded action as symbol; one of `:on`, `:off`, `push` or `:timer`.
             action `:push` will switch on for 200ms to simulate a push.
-            action `:timer` will switch off after timer secs.
-- `timer`: if action == `timer`, the device is switched on and
-           off again after `timer` secs (default 10s).
 
 For the API-doc of the Shelly devices see:
 `<https://shelly-api-docs.shelly.cloud>`.
 """
-function switch_shelly_25_relay(ip, relay, action; timer = 10)
+function switch_shelly_25_relay(ip, relay, action; 
+            port=nothing, user=nothing, password=nothing)
 
     print_log("Switching Shelly1/2.5 $ip with action: $action")
-    success = false
-    timeout = 10
+
+    if !isnothing(port)
+        ip = "$ip:$port"
+    end
+    if !isnothing(user) && !isnothing(password)
+        ip = "$user:$password@$ip"
+    end
+    url = "http://$ip/relay/$relay"
+
+    success = true
     if action == :on
-        cmd = `curl -v -m $timeout "http://$ip/relay/$relay?turn=on"`
-        success = tryrun(cmd)
+        cmd = "turn=on"
     elseif action == :timer
-        cmd = `curl -v -m $timeout "http://$ip/relay/$relay?turn=on&timer=$timer"`
-        success = tryrun(cmd)
+        cmd = "turn=on&timer=$timer"
     elseif action == :off
-        cmd = `curl -v -m $timeout "http://$ip/relay/$relay?turn=off"`
-        success = tryrun(cmd)
+        cmd = "turn=off"
     elseif action == :push
-        cmd = `curl -v -m $timeout "http://$ip/relay/$relay?turn=on"`
-        success = tryrun(cmd)
-        sleep(1)
-        cmd = `curl -v -m $timeout "http://$ip/relay/$relay?turn=off"`
-        success = tryrun(cmd)
+        cmd = "turn=on"
     else
         print_log("ERROR in switch_shelly_25: action $action is not supported")
         publish_say("Try to switch a Shelly-two point five with an unsupported command!")
+        success = false
     end
-
-    return(success)
+    try
+        HTTP.get("$url?$cmd")
+        if action == :push
+            sleep(0.2)
+            HTTP.get("$url?turn=off")
+        end
+    catch
+        print_log("ERROR in switch_shelly_25: HTTP GET not successful!")
+        publish_say("Try to switch a Shelly-two point five was not successful!")
+        success = false
+    end
+        
+    return success
 end
 
 
 
 """
-    move_shelly_25_roller(ip, action; pos = 100, duration = 1)
+    move_shelly_25_roller(ip, action; pos=100, 
+            port=nothing, user=nothing, password=nothing)
 
 Move a roller with a shelly2.5 device with IP `ip`
 and return `true`, if successful.
@@ -93,27 +109,43 @@ and return `true`, if successful.
 - `ip`: IP address or DNS name of Shelly2.5 device
 - `action`: demanded action as symbol; one of `:open`, `:close`, `:stop`
             or `:to_pos`.
-- `pos`: desired position in percent
-- 'duration': is not yet implemented.
+- `pos`: desired position in percent.
 
 For the API-doc of the Shelly devices see:
 `<https://shelly-api-docs.shelly.cloud>`.
 """
-function move_shelly_25_roller(ip, action; pos = 100, duration = 1)
+function move_shelly_25_roller(ip, action; pos=100,
+            port=nothing, user=nothing, password=nothing)
 
-    timeout = 10
+    if !isnothing(port)
+        ip = "$ip:$port"
+    end
+    if !isnothing(user) && !isnothing(password)
+        ip = "$user:$password@$ip"
+    end
+    url = "http://$ip/relay/$relay"
+
+    success = true
     if action == :open
-        cmd = `curl -v -m $timeout "http://$ip/roller/0?go=open"`
+        cmd = "go=open"
     elseif action == :close
-        cmd = `curl -v -m $timeout "http://$ip/roller/0?go=close"`
+        cmd = "go=close"
     elseif action == :stop
-        cmd = `curl -v -m $timeout "http://$ip/roller/0?go=stop"`
+        cmd = "go=stop"
     elseif action == :to_pos
-        cmd = `curl -v -m $timeout "http://$ip/roller/0?go=to_pos&roller_pos=$pos"`
+        cmd = "go=to_pos&roller_pos=$pos"
     else
-        print_log("ERROR in switchShelly25: action $action is not supported")
-        publish_say("Try to move a Shelly-two point five with an unsupported command!")
+        print_log("ERROR in switch_shelly_25_roller: action $action is not supported")
+        publish_say("Try to switch a Shelly-two point five with an unsupported command!")
+        success = false
+    end
+    try
+        HTTP.get("$url?$cmd")
+    catch
+        print_log("ERROR in switch_shelly_25: HTTP GET not successful!")
+        publish_say("Try to switch a Shelly-two point five was not successful!")
+        success = false
     end
 
-    return(tryrun(cmd))
+    return success
 end
